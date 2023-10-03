c0946f0ed53549b9b01626fceb709d0bd2efa93c
ayee-lucas <nouminosxz@gmail.com>
2023-10-03 14:31:15 -0600

    Added skill icons

---
+++ src/components/skills.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+    import Icon from "@iconify/svelte";
+
+</script>
+
+<section class="w-full flex items-center">
+    <ul class="flex flex-wrap items-center justify-center  gap-10 p-4">
+        <li>
+            <Icon icon="skill-icons:nextjs-light" class="text-4xl" />
+        </li>
+        <li>
+            <Icon icon="skill-icons:angular-light" class="text-4xl" />
+        </li>
+        <li>
+            <Icon icon="skill-icons:expressjs-light" class="text-4xl" />
+        </li>
+        <li>
+            <Icon icon="skill-icons:nodejs-light" class="text-4xl" />
+        </li>
+        <li>
+            <Icon icon="devicon:typescript" class="text-4xl" />
+        </li>
+        <li>
+            <Icon icon="skill-icons:javascript" class="text-4xl" />
+        </li>
+        <li>
+            <Icon icon="skill-icons:golang" class="text-4xl" />
+        </li>
+        <li>
+            <Icon icon="skill-icons:html" class="text-4xl" />
+        </li>
+        <li>
+            <Icon icon="skill-icons:css" class="text-4xl" />
+        </li>
+        <li>
+            <Icon icon="skill-icons:mongodb" class="text-4xl" />
+        </li>
+        <li>
+            <Icon icon="skill-icons:docker" class="text-4xl" />
+        </li>
+        <li>
+            <Icon icon="skill-icons:git" class="text-4xl" />
+        </li>
+        <li>
+            <Icon icon="skill-icons:tailwindcss-light" class="text-4xl" />
+        </li>
+        <li>
+            <Icon icon="skill-icons:github-light" class="text-4xl" />
+        </li>
+        <li>
+            <Icon icon="skill-icons:gitlab-light" class="text-4xl" />
+        </li>
+        <li>
+            <Icon icon="skill-icons:linux-light" class="text-4xl" />
+        </li>
+    </ul>
+
+
+</section>
