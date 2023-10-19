edd70330dba2dc3b2ca7b14c4ee81da438b57202
Alan Lopez <108323739+ayee-lucas@users.noreply.github.com>
2023-10-19 05:07:43 +0000

    removed image

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -9,7 +9,6 @@
 />
 
 <script>
-    import me from "$lib/images/me.jpeg"
     import bg from "$lib/images/bgwebp.webp"
     import Skills from "../../components/skills.svelte";
 </script>
@@ -51,34 +50,7 @@
       <div
         class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64"
       >
-        <div class="px-6">
-          <div class="flex flex-wrap justify-center">
-            <div class="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
-              <div class="relative">
-                <img
-                  alt="..."
-                  src={me}
-                  width="300"
-                  height="300"
-                  class="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"
-                />
-              </div>
-            </div>
-            <div
-              class="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center"
-            >
-              <div class="py-6 px-3 mt-32 sm:mt-0">
-                <a
-                  href="https://www.linkedin.com/in/alan-lopez-4a8aa2263"
-                  class="bg-black active:bg-zinc-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
-                  type="button"
-                >
-                  Connect
-                </a>
-              </div>
-            </div>
-
-          </div>
+        <div class="px-6 w-full">
           <div class="text-center mt-12">
             <h3
               class="text-4xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2"
@@ -100,6 +72,15 @@
               <i
                 class="fas fa-university mr-2 text-lg text-blueGray-400"
               />Fundacion Kinal
+            </div>
+            <div class="py-3">
+                <a
+                  href="https://www.linkedin.com/in/alan-lopez-4a8aa2263"
+                  class="bg-black active:bg-zinc-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+                  type="button"
+                >
+                  Connect
+                </a>
             </div>
           </div>
           <Skills />
